a6074e82272acabcfaafb11dca75eb6338e11e5c
Merter Çoban <90144140+mertercoban@users.noreply.github.com>
2024-05-12 23:38:34 +0300

    bx-slider

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#slider1").bxSlider({
+        auto: true,
+        moveSlides: 1,
+        pause: 3000,
+        pager: false,
+        controls: false,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 750,
+        marginLeft: 10,
+        randomStart: true,
+    });
+    console.log("qweqwdwa");
+});
